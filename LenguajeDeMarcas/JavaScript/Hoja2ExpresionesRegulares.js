a15806155c6cd19e5d5496e5c5ecc3500c816585
Jose Maria De Lucas Plata <d3lucasx@gmail.com>
2025-04-02 14:06:46 +0200

    js

--- LenguajeDeMarcas/JavaScript/Hoja2ExpresionesRegulares.js
+++ LenguajeDeMarcas/JavaScript/Hoja2ExpresionesRegulares.js
@@ -1,3 +1,5 @@
+//EJERCICIO 1.
+
 function isFecha(fecha){
     //Formato de la fecha dd,mm,aaaa
     const regex = /^(0[1-9]|[1-2][0-9]|3[0-1]),(0[1-9]|1[0-2]),(19|20\d{2})$/; //^[0-9]{2},\d{2},\d{4}$/
@@ -22,17 +24,43 @@ function Ejercicio1(){
             document.writeln("<h2> false --> " +fecha+ "</h2>");
         }
 }
-function isCorreo(correo){
-    let correo = prompt("Introduce tu correo: ");
-}
-function Ejercicio2(){
-let expReg= /\b[A-Za-z0-9.&_]+\@[A-Za-z]+\.[a-z]{2,3}\b/;
-let correo =prompt("Escribeme un correo.");
 
-    if (expReg.test(palabra)){
+//EJERCICIO 2
+
+function isCorreo(correo){
+    let expReg= /\b[A-Za-z0-9.&_]+\@[A-Za-z]+\.[a-z]{2,3}\b/;
+    if(regex.test(correo)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function Ejercicio2(){
+    let correo = prompt("Introduce tu correo: ");
+    if (isCorreo(correo)){
         document.writeln("<h2> true -->" +correo +"</h2>");
     }else{
         document.writeln("<h2> false --> " +correo+ "</h2>");
+    }
+}
+
+function is telefono(telefono){
+    let expReg= /\b\d{2}\-\d{3}\-\d{2}\-\d{2}\-\d{2}\b/;
+    if(regex.test(telefono)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function Ejercicio3(){
+    let telefono = prompt("Introduce tu telefono: ");
+    
+    if (isCorreo(telefono)){
+        document.writeln("<h2> true -->" +telefono +"</h2>");
+    }else{
+        document.writeln("<h2> false --> " +telefono+ "</h2>");
     }
 }
 
